31554eed44db96572f01b66878c376959a524cfe
Vitor Gonçalves <vitorg@uchicago.edu>
2026-02-05 16:12:51 -0600

    fix styles link and favicon

--- .eleventy.js
+++ .eleventy.js
@@ -15,6 +15,7 @@ module.exports = function(eleventyConfig) {
     },
     templateFormats: ["html", "md", "njk"],
     htmlTemplateEngine: "njk",
-    markdownTemplateEngine: "njk"
+    markdownTemplateEngine: "njk",
+    pathPrefix: "/brainstorm-mockups/"
   };
 };
